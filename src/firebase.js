8f963e2f469bde258883abd56d4e0556b9ddcced
Anna Kutova <annnakutova@gmail.com>
2025-09-07 12:47:34 +0300

    add.env file

--- src/firebase.js
+++ src/firebase.js
@@ -1,16 +1,16 @@
-// src/firebase.js
 import { initializeApp } from 'firebase/app';
 import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDo1hx4dX-IqNr3lNGas-iOzwg2qhkfbtE",
-    authDomain: "recommend-u.firebaseapp.com",
-    projectId: "recommend-u",
-    storageBucket: "recommend-u.firebasestorage.app",
-    messagingSenderId: "871928992577",
-    appId: "1:871928992577:web:fb67a34cfd836e4c8a47c0"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
+
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const provider = new GoogleAuthProvider();
